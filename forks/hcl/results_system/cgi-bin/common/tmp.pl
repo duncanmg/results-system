bc020be5f271866ecc4bbfd7a42c1f102cf0c696
Duncan Garland <duncan@debian2.g072637711>
2014-12-31 13:00:11 +0000

    Clear the decks for version 3.

--- forks/hcl/results_system/cgi-bin/common/tmp.pl
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/perl
-
-use CGI;
-my $cgi = CGI->new();
-
-print $cgi->header;
-print $cgi->start_html;
-print "Hello";
-print $cgi->end_html;
--- forks/nfcca/cgi-bin/results_system/common/CreatePodHTML.bat
+++
@@ -1,6 +0,0 @@
-echo hello %1
-REM echo goodbye
-REM call pod2html -title "Fixtures.pm" -css "gen_styles.css" -outfile "..\..\documentation\Fixtures.htm" -infile Fixtures.pm
-REM echo here
-call pod2html -title "%1.pm" -css "gen_styles.css" -outfile "..\..\documentation\%1.htm" -infile %1.pm
-REM echo there
