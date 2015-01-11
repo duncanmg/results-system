bab54957033542d2c7f24ed2602124d626170b15
Duncan Garland <duncan@debian2.g072637711>
2015-01-11 20:56:04 +0000

    Little bit more work on IO.

--- ResultsSystem/lib/ResultsSystem/IO/XML.pm
+++ ResultsSystem/lib/ResultsSystem/IO/XML.pm
@@ -9,14 +9,39 @@ use Path::Class;
 use XML::Simple qw/:strict/;
 use Params::Validate qw/:all/;
 
+=head1 Attributes
+
+=over
+
+=item backup_dir
+
+=item backup_ext
+
+=item write_dir
+
+=item full_filename
+
+=back
+
+=cut
+
 has 'backup_dir' => ( 'is' => 'ro', 'default' => sub {"."} );
 has 'backup_ext' => ( 'is' => 'ro', 'default' => sub { time() . "" } );
 has 'write_dir'  => ( 'is' => 'ro', 'default' => sub {"."} );
-has 'full_filename' => ( 'is' => 'ro', 'required' => 1 );
+has 'full_filename' => ( 'is' => 'ro' );
+
+=head1 Internal Methods
+
+=cut
+
+=head2 _backup
+
+=cut
 
 sub _backup {
   my $self = shift;
   my ( $name, $path, $suffix ) = fileparse( $self->full_filename );
+  die "full_filename is not defined" if !$self->full_filename;
   copy(
     $self->full_filename,
     Path::Class::File->new(
@@ -26,25 +51,149 @@ sub _backup {
   return 1;
 }
 
+=head2 _write
+
+=cut
+
 sub _write {
   my ( $self, $input ) = validate_pos( @_, 1, 1 );
   my $FP;
   my $xml = XML::Simple->new();
-  my $out = $xml->XMLout( $input, KeepRoot => 1, KeyAttr => [] );
+  my $out = $xml->XMLout(
+    $input,
+    KeepRoot => 1,
+    KeyAttr  => [],
+    RootName => "Result"
+  );
+  die "full_filename is not defined" if !$self->full_filename;
   open( $FP, '>', $self->full_filename ) || die $!;
   print $FP $out || die $!;
   close $FP || die $!;
   return 1;
 }
 
+=head2 _read
+
+=cut
+
 sub _read {
   my ($self) = validate_pos( @_, 1 );
   my $FP;
+  die "full_filename is not defined" if !$self->full_filename;
   open( $FP, $self->full_filename ) || die $!;
-  my @lines = readline($FP) || die $!;
-  my $xml   = XML::Simple->new();
-  my $in    = $xml->XMLin( join( "\n", @lines ) );
+  my @lines = ();
+  while (<$FP>) {
+    push @lines, $_;
+  }
+  my $xml = XML::Simple->new();
+  my $in = $xml->XMLin( join( "\n", @lines ), ForceArray => 1, KeyAttr => [] );
   return $in;
 }
+
+=head1 Example Perl
+
+  my $VAR1 = {
+    'match' => [
+      { 'match_date'   => [ '21-Jan-2014' ],
+        'played'       => [ 'Y' ],
+        'away'         => [ 'Waterlooville' ],
+        'away_details' => [
+          { 'runs_scored'    => [ '100' ],
+            'wickets_lost'   => [ '5' ],
+            'bowling_points' => [ '3' ],
+            'penalty_points' => [ '0' ],
+            'batting_points' => [ '5' ],
+            'result'         => [ 'W' ]
+          }
+        ],
+        'home_details' => [
+          { 'runs_scored'    => [ '100' ],
+            'wickets_lost'   => [ '5' ],
+            'bowling_points' => [ '3' ],
+            'penalty_points' => [ '0' ],
+            'batting_points' => [ '5' ],
+            'result'         => [ 'W' ]
+          }
+        ],
+        'home' => [ 'Purbrook' ]
+      },
+      { 'match_date'   => [ '21-Jan-2014' ],
+        'played'       => [ 'Y' ],
+        'away'         => [ 'P\'mouth & S\'sea' ],
+        'away_details' => [
+          { 'runs_scored'    => [ '100' ],
+            'wickets_lost'   => [ '5' ],
+            'bowling_points' => [ '3' ],
+            'penalty_points' => [ '0' ],
+            'batting_points' => [ '5' ],
+            'result'         => [ 'W' ]
+          }
+        ],
+        'home_details' => [
+          { 'runs_scored'    => [ '100' ],
+            'wickets_lost'   => [ '5' ],
+            'bowling_points' => [ '3' ],
+            'penalty_points' => [ '0' ],
+            'batting_points' => [ '5' ],
+            'result'         => [ 'W' ]
+          }
+        ],
+        'home' => [ 'Fareham & Crofton' ]
+      }
+    ]
+  };
+
+=cut
+
+=head1 Example XML
+
+  <result_set>
+  <match>
+          <home>Purbrook</home>
+          <away>Waterlooville</away>
+          <match_date>21-Jan-2014</match_date>
+          <played>Y</played>
+          <home_details>
+                  <runs_scored>100</runs_scored>
+                  <wickets_lost>5</wickets_lost>
+                  <batting_points>5</batting_points>
+                  <bowling_points>3</bowling_points>
+                  <penalty_points>0</penalty_points>
+                  <result>W</result>
+          </home_details>
+          <away_details>
+                  <runs_scored>100</runs_scored>
+                  <wickets_lost>5</wickets_lost>
+                  <batting_points>5</batting_points>
+                  <bowling_points>3</bowling_points>
+                  <penalty_points>0</penalty_points>
+                  <result>W</result>
+          </away_details>
+  </match>
+  <match>
+          <home>Fareham &amp; Crofton</home>
+          <away>P'mouth &amp; S'sea</away>
+          <match_date>21-Jan-2014</match_date>
+          <played>Y</played>
+          <home_details>
+                  <runs_scored>100</runs_scored>
+                  <wickets_lost>5</wickets_lost>
+                  <batting_points>5</batting_points>
+                  <bowling_points>3</bowling_points>
+                  <penalty_points>0</penalty_points>
+                  <result>W</result>
+          </home_details>
+          <away_details>
+                  <runs_scored>100</runs_scored>
+                  <wickets_lost>5</wickets_lost>
+                  <batting_points>5</batting_points>
+                  <bowling_points>3</bowling_points>
+                  <penalty_points>0</penalty_points>
+                  <result>W</result>
+          </away_details>
+  </match>
+  </result_set>
+
+=cut
 
 1;
--- ResultsSystem/t/04-load.t
+++ ResultsSystem/t/04-load.t
@@ -4,17 +4,84 @@ use Test::More;
 use Test::Exception;
 use Cwd qw/cwd/;
 use Path::Class;
+use Data::Dumper;
+use Test::Deep;
 
 use_ok('ResultsSystem::IO::XML');
 
-my $f = Path::Class::File->new( cwd(), "t", "test.xml" );
+my $f = Path::Class::File->new( cwd(), "t", "result.xml" );
 
 my $obj = ResultsSystem::IO::XML->new( full_filename => $f );
 isa_ok( $obj, 'ResultsSystem::IO::XML' );
 
-my $data = { one => 1, two => 2, three => [ 4, 5, 6 , 7 ] };
+my $data = {
+  'match' => [
+    { 'match_date'   => ['21-Jan-2014'],
+      'played'       => ['Y'],
+      'away'         => ['Waterlooville'],
+      'away_details' => [
+        { 'runs_scored'    => ['100'],
+          'wickets_lost'   => ['5'],
+          'bowling_points' => ['3'],
+          'penalty_points' => ['0'],
+          'batting_points' => ['5'],
+          'result'         => ['W']
+        }
+      ],
+      'home_details' => [
+        { 'runs_scored'    => ['100'],
+          'wickets_lost'   => ['5'],
+          'bowling_points' => ['3'],
+          'penalty_points' => ['0'],
+          'batting_points' => ['5'],
+          'result'         => ['W']
+        }
+      ],
+      'home' => ['Purbrook']
+    },
+    { 'match_date'   => ['21-Jan-2014'],
+      'played'       => ['Y'],
+      'away'         => ['P\'mouth & S\'sea'],
+      'away_details' => [
+        { 'runs_scored'    => ['100'],
+          'wickets_lost'   => ['5'],
+          'bowling_points' => ['3'],
+          'penalty_points' => ['0'],
+          'batting_points' => ['5'],
+          'result'         => ['W']
+        }
+      ],
+      'home_details' => [
+        { 'runs_scored'    => ['100'],
+          'wickets_lost'   => ['5'],
+          'bowling_points' => ['3'],
+          'penalty_points' => ['0'],
+          'batting_points' => ['5'],
+          'result'         => ['W']
+        }
+      ],
+      'home' => ['Fareham & Crofton']
+    }
+  ]
+};
 
-$obj->_write($data);
+#$obj->_write($data);
+
+my $data_read = $obj->_read();
+cmp_deeply( $data_read, $data, "The data read from result.xml is as expected." )
+  || diag( Dumper $data_read);
+
+$obj = ResultsSystem::IO::XML->new( full_filename => "./t/result_output.xml" );
+isa_ok( $obj, 'ResultsSystem::IO::XML' );
+
+ok( $obj->_write( { result_set => $data_read } ), "Wrote data to file result_output.xml as xml" );
+
+$obj = ResultsSystem::IO::XML->new( full_filename => "./t/result_output.xml" );
+isa_ok( $obj, 'ResultsSystem::IO::XML' );
+
+$data_read = $obj->_read();
+cmp_deeply( $data_read, $data, "The data read from result.xml is as expected." )
+  || diag( Dumper $data_read);
 
 done_testing;
 
