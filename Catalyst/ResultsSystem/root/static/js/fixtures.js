e460ec5d299b65b8652d0edf916fe33541b1b035
Duncan Garland <duncan.garland@ntlworld.com>
2015-12-13 18:56:15 +0000

    Work on client side validation and simple css

---
+++ Catalyst/ResultsSystem/root/static/js/fixtures.js
@@ -0,0 +1,21 @@
+<script>
+		jQuery(document).ready(function(){
+			// binds form submission and fields to the validation engine
+			jQuery("#enter_results").validationEngine();
+		});
+
+		/**
+		*
+		* @param {jqObject} the field where the validation applies
+		* @param {Array[String]} validation rules for this field
+		* @param {int} rule index
+		* @param {Map} form options
+		* @return an error string if validation failed
+		function checkHELLO(field, rules, i, options){
+			if (field.val() != "HELLO") {
+				// this allows to use i18 for the error msgs
+				return options.allrules.validate2fields.alertText;
+			}
+		}
+		*/
+	</script>
