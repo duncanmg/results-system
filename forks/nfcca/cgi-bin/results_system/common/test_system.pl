039556ce475d677355d418d82bfaa41cd32cbd8f
Duncan Garland <duncan.garland@ntlworld.com>
2018-03-21 08:34:33 +0000

    Fixed Perl::Critic violations. Updated module list. Use require_ok instead of custom function. Improved readability of output.

--- forks/nfcca/cgi-bin/results_system/common/test_system.pl
+++ forks/nfcca/cgi-bin/results_system/common/test_system.pl
@@ -2,52 +2,38 @@
 
 use CGI;
 use strict;
+use warnings;
+
 use Test::More;
 
-unshift @INC, "../common";
-
-# *******************************************************
-sub can_use {
-
-  # *******************************************************
-  my $module = shift;
-  my $err    = 0;
-  my $msg    = "ok";
-  eval { require $module; };
-  if ($@) {
-    $err = 1;
-    $msg = $@;
-  }
-  return ( $err, $msg );
+BEGIN {
+  use LoadEnv;
+  LoadEnv::run();
 }
 
 # *******************************************************
 sub test_module_availability {
 
   # *******************************************************
-  my ( $ret, $msg );
   my $err         = 0;
-  my @module_list = (    # Standard Perl modules
-    "CGI.pm",           "Test/More.pm", "Slurp.pm",
-    "Regexp/Common.pm", "Test/Harness.pm",
-
-    # Custom modules for project
-    "Fcutils2.pm",    "WeekFixtures.pm", "WeekData.pm",
-    "Slurp.pm",       "ResultsIndex.pm", "ResultsConfiguration.pm",
-    "Pwd.pm",         "Parent.pm",       "Menu.pm",
-    "LeagueTable.pm", "Fixtures.pm",     "TablesIndex.pm"
-
-      # Test scripts
-  );
+  my @module_list = qw!
+    ResultsSystem::Model ResultsSystem::Locker ResultsSystem::Factory ResultsSystem::Router ResultsSystem::Exception ResultsSystem::Logger
+    ResultsSystem::Starter ResultsSystem::View ResultsSystem::View::Menu ResultsSystem::View::MenuJs ResultsSystem::View::SaveResults
+    ResultsSystem::View::Frame ResultsSystem::View::Week ResultsSystem::View::TablesIndex ResultsSystem::View::Blank ResultsSystem::View::LeagueTable
+    ResultsSystem::View::Pwd ResultsSystem::View::Week::Results ResultsSystem::View::Week::FixturesForm ResultsSystem::View::ResultsIndex
+    ResultsSystem::View::Message ResultsSystem::Model::Menu ResultsSystem::Model::Fixtures ResultsSystem::Model::MenuJs ResultsSystem::Model::WeekData::Writer
+    ResultsSystem::Model::WeekData::Reader ResultsSystem::Model::SaveResults ResultsSystem::Model::Frame ResultsSystem::Model::WeekData
+    ResultsSystem::Model::TablesIndex ResultsSystem::Model::LeagueTable ResultsSystem::Model::Pwd ResultsSystem::Model::ResultsIndex
+    ResultsSystem::Model::SaveWeekData ResultsSystem::Model::WeekFixtures ResultsSystem::AutoCleaner ResultsSystem::Configuration
+    ResultsSystem::Controller::Menu ResultsSystem::Controller::MenuJs ResultsSystem::Controller::SaveResults ResultsSystem::Controller::Frame
+    ResultsSystem::Controller::TablesIndex ResultsSystem::Controller::Blank ResultsSystem::Controller::ResultsIndex ResultsSystem::Controller::WeekFixtures
+    !;
 
   foreach my $m (@module_list) {
-
-    ( $ret, $msg ) = can_use($m);
-    ok( $ret == 0, "Module $m is available. $msg.<br/>" );
-    if ( $ret != 0 ) {
-      $err = 1;
-    }
-
+    print "<p>\n";
+    my $ok = require_ok($m);
+    print "</p>\n";
+    print "<p style='color:red'>The last test failed.</p>" if !$ok;
   }
   return $err;
 }
@@ -78,3 +64,4 @@ test_module_availability;
 print $builder->_ending;
 
 print $q->end_html;
+
